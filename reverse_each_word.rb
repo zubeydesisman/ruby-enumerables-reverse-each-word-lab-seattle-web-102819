0fba0780c7eade7508c0b5caf965b3ec15f57c0f
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-02 21:02:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   split_string = string.split( )
-  split_string.each { |item| item.reverse}
+  split_string.map { |item| item.reverse}
     puts split_string.join()
   
 end
